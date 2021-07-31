a779c17d847646a94e692f907c7b3c48e07c3682
root <ralexrivero@gmail.com>
2021-07-30 17:38:07 -0700

    more cases for conjugate

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,13 +8,27 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
-		printf("%g\n", c.re);
-	else if (c.im < 0)
-	{
-		c.im = -c.im;
-		printf("%g - %gi\n", c.re, c.im);
-	}
+/* testing for conjugate */
+if (c.re == 0)
+{
+	if (c.im < 0)
+		printf("- %.9gi\n", c.im * (-1));
+	else if (c.im == 0)
+		printf("0\n");
 	else
-		printf("%g + %gi\n", c.re, c.im);
+		printf("%.9g\n", c.im);
+}
+else
+{
+	if (c.im < -1)
+		printf("%.9g - %.9gi\n", c.re, c.im * (-1));
+	else if (c.im == -1)
+		printf("%.9g - i\n", c.re);
+	else if (c.im == 0)
+		printf("%.9g\n", c.re);
+	else if (c.im == 1)
+		printf("%.9g + i\n", c.re);
+	else
+		printf("%.9g + %.9gi\n", c.re, c.im);
+}
 }
